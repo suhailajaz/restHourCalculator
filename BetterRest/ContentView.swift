47a264d351307e07eaad239cb73315d638021f37
suhail <suhailajaz338@gmail.com>
2022-12-08 14:25:24 +0530

    new commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,13 +5,89 @@
 //  Created by suhail on 20/06/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmout = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? Date.now
+        
     }
+   
+    var body: some View {
+        NavigationView{
+            Form{
+                VStack(alignment:.leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmout.formatted()) hours", value:$sleepAmout, in:4...12, step:0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" :"\(coffeeAmount) cups", value:$coffeeAmount, in: 1...20)
+                }
+                    
+            }.navigationTitle("Better Rest")
+             .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    Button("Calculate", action: calculateBedTime)
+                }
+                .alert(alertTitle,isPresented: $showingAlert){
+                    Button("OK"){}
+                }message: {
+                    Text(alertMessage)
+                }
+        }
+       
+    }
+    
+    func calculateBedTime(){
+        
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+         
+            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmout, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted , time: .shortened)
+        }
+            catch{
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
